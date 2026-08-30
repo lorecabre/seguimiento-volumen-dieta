3eb84f02db309e27344e333e5f19f1ff390730fb
lorecabre <lorecabre87@gmail.com>
2026-08-30 10:03:57 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='seguimiento-volumen-v3';
+const CACHE='seguimiento-volumen-v4';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
